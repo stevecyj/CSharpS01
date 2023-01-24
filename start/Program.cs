3fd0f760ba2ed2e8304c115367c21cbdef245cb9
Steve <ycsteven.tsao@gmail.com>
2023-01-25 05:37:19 +0800

    init: start

---
+++ start/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Start
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,\n World!");
+            Console.WriteLine("Hello,\t World!");
+        }
+    }
+}
