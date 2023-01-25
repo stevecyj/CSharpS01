7804eb387d798560c88188637457db6b96465b49
Steve <ycsteven.tsao@gmail.com>
2023-01-25 17:33:21 +0800

    字串替代標記，格式化輸出

--- variable-research/Program.cs
+++ variable-research/Program.cs
@@ -1,12 +1,16 @@
-﻿int a=5,b=10;
+﻿// int a = 5, b = 10;
 /*
 int temp=a;
 a=b;
 b=temp;
 */
 
-a = a + b;
+/*a = a + b;
 b = a - b;
 a = a - b;
 Console.WriteLine(a);
-Console.WriteLine(b);
+Console.WriteLine(b);*/
+
+int a = 23, b = 45;
+Console.WriteLine(a + "+" + b + "=" + (a + b));
+Console.WriteLine("{0}+{1}={2}", a, b, a + b);
