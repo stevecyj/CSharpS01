d327bda767c290dd72e41c660a4fe02a589f2874
Steve <ycsteven.tsao@gmail.com>
2023-01-25 22:50:19 +0800

    reverse digits

---
+++ pratice-reverse-digits/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int unitDigit = number % 10;
+int tensDigit = number / 10 % 10;
+int hundredsDigit = number / 100;
+Console.WriteLine("" + unitDigit + tensDigit + hundredsDigit);
