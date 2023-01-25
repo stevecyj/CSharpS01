d292d9379494da9a295289eafe776f2d8f5050a6
Steve <ycsteven.tsao@gmail.com>
2023-01-25 21:32:48 +0800

    logic operator pratice

--- logic-operator/Program.cs
+++ logic-operator/Program.cs
@@ -1,7 +1,11 @@
-﻿bool a = (3<4)&&(9<6);
+﻿/*bool a = (3<4)&&(9<6);
 bool b = (3<4)&&(9<10);
 bool c = !(4 < 7);
 
 Console.WriteLine(a);
 Console.WriteLine(b);
-Console.WriteLine(c);
+Console.WriteLine(c);*/
+
+int age = Convert.ToInt32(Console.ReadLine());
+bool isYouth = age >= 18 && age <= 60;
+Console.WriteLine(isYouth);
