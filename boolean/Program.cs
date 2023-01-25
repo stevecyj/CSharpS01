86bcfda82a1ba5a75eb163b147bd1e7e4891655c
Steve <ycsteven.tsao@gmail.com>
2023-01-25 21:03:03 +0800

    relational operator

---
+++ boolean/Program.cs
@@ -0,0 +1,12 @@
+﻿bool a = 45 == 67;
+bool b = 45 < 67;
+bool c = 45 <= 67;
+bool d = 45 > 67;
+bool e = 45 >= 67;
+bool f = 45 != 67;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
