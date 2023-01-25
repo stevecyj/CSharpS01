de36c43d1d72d810029c4532663111910b571805
Steve <ycsteven.tsao@gmail.com>
2023-01-25 15:36:51 +0800

    practice

---
+++ practice/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int c = a + b;
+Console.WriteLine(c);
+*/
+
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+// int c=(a+b)/2;
+int c=a/b;
+Console.WriteLine(c);
