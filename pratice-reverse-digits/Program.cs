2326e919b765ea05c6467e955c6e3d1298ab14b3
Steve <ycsteven.tsao@gmail.com>
2023-01-25 23:09:57 +0800

    encrypt and decode

--- pratice-reverse-digits/Program.cs
+++ pratice-reverse-digits/Program.cs
@@ -1,5 +1,12 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
+﻿/*int number = Convert.ToInt32(Console.ReadLine());
 int unitDigit = number % 10;
 int tensDigit = number / 10 % 10;
 int hundredsDigit = number / 100;
-Console.WriteLine("" + unitDigit + tensDigit + hundredsDigit);
+Console.WriteLine("" + unitDigit + tensDigit + hundredsDigit);*/
+
+int num = Convert.ToInt32(Console.ReadLine());
+int tensDigit = num / 10 % 10;
+int thousandsDigit = num / 1000 % 10;
+int numNew = thousandsDigit * 10 + tensDigit;
+char c=(char)numNew;
+Console.WriteLine(c);
