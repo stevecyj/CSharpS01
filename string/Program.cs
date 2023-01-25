c44c780df57642574273faaca122d20e068564f3
Steve <ycsteven.tsao@gmail.com>
2023-01-25 14:06:00 +0800

    string operate

--- string/Program.cs
+++ string/Program.cs
@@ -1,5 +1,8 @@
-﻿int a=97;
-char b=(char)a;
+﻿char a='1';
+int b=a;
 
 Console.WriteLine(a);
 Console.WriteLine(b);
+string str = @"c:\a\b\c";
+Console.WriteLine(str);
+Console.WriteLine("www"+123);
