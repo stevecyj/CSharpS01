1ce6755080cfc4aa2c5617e43e935e4b6c9049ff
Steve <ycsteven.tsao@gmail.com>
2023-01-25 23:19:06 +0800

    increment

--- pratice-reverse-digits/Program.cs
+++ pratice-reverse-digits/Program.cs
@@ -4,9 +4,21 @@ int tensDigit = number / 10 % 10;
 int hundredsDigit = number / 100;
 Console.WriteLine("" + unitDigit + tensDigit + hundredsDigit);*/
 
-int num = Convert.ToInt32(Console.ReadLine());
+/*int num = Convert.ToInt32(Console.ReadLine());
 int tensDigit = num / 10 % 10;
 int thousandsDigit = num / 1000 % 10;
 int numNew = thousandsDigit * 10 + tensDigit;
 char c=(char)numNew;
-Console.WriteLine(c);
+Console.WriteLine(c);*/
+
+/*
+int a=3;
+int b=a++ + a++;
+Console.WriteLine(b);
+Console.WriteLine(a);
+*/
+
+int a = 3;
+int b = a++ + ++a;
+Console.WriteLine(b);
+Console.WriteLine(a);
