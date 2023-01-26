c81664b4ef665f1a3ce61cf2fd14b472efd221e9
Steve <ycsteven.tsao@gmail.com>
2023-01-26 17:05:33 +0800

    while 1 to 100

--- while-sentence/Program.cs
+++ while-sentence/Program.cs
@@ -7,9 +7,25 @@ while (i < 10)
 
 Console.WriteLine("after while " + i);*/
 
-int HP = 100;
+/*int HP = 100;
 while (HP > 0)
 {
     HP -= 3;
     Console.WriteLine("HP" + HP);
+}*/
+
+/*int i = 100;
+while (i > 0)
+{
+    Console.WriteLine(i);
+    i--;
+}*/
+
+int i=1;
+int sum = 0;
+while (i < 101)
+{
+    sum += i;
+    i++;
 }
+Console.WriteLine(sum);
