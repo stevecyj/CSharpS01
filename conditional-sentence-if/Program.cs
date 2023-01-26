74f5bcad256cc4fef37b97de11a215eeb89a1453
Steve <ycsteven.tsao@gmail.com>
2023-01-26 09:20:26 +0800

    nested if sentence

--- conditional-sentence-if/Program.cs
+++ conditional-sentence-if/Program.cs
@@ -1,4 +1,4 @@
-﻿int age = Convert.ToInt32(Console.ReadLine());
+﻿/*int age = Convert.ToInt32(Console.ReadLine());
 if (age <= 16)
 {
     Console.WriteLine("無法進入");
@@ -7,4 +7,32 @@ else
 {
     Console.WriteLine("可以進入");
 }
-Console.WriteLine("finish");
+Console.WriteLine("finish");*/
+
+/*int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 1)
+{
+    Console.WriteLine("奇數");
+}
+else
+{
+    Console.WriteLine("偶數");
+}*/
+
+int age = Convert.ToInt32(Console.ReadLine());
+if (age >= 18 && age <= 30)
+{
+    Console.WriteLine("可以參加活動");
+    if (age % 2 == 1)
+    {
+        Console.WriteLine("獲得獎品");
+    }
+    else
+    {
+        Console.WriteLine("沒有獲得獎品");
+    }
+}
+else
+{
+    Console.WriteLine("不可以參加活動");
+}
