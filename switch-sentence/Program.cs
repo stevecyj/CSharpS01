1b059c96c50d501a8021d6a3fea535f4c9e5eb22
Steve <ycsteven.tsao@gmail.com>
2023-01-26 14:35:20 +0800

    switch sentence

---
+++ switch-sentence/Program.cs
@@ -0,0 +1,22 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.WriteLine("礦泉水");
+        break;
+    case 2:
+        Console.WriteLine("紅茶");
+        break;
+    case 3:
+        Console.WriteLine("綠茶");
+        break;
+    case 4:
+        Console.WriteLine("雪碧");
+        break;
+    case 5:
+        Console.WriteLine("可口可樂");
+        break;
+    default:
+        Console.WriteLine("脈動");
+        break;
+}
